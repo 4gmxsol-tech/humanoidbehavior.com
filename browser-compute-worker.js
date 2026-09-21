a6b90279393dc2f5178ce317c60202c98f1a5916
4gmxsol-tech <4gmxsol@gmail.com>
2026-09-21 11:38:50 +0200

    Run browser MuJoCo compute off the UI thread

---
+++ browser-compute-worker.js
@@ -0,0 +1,13 @@
+import { runBrowserEvaluation } from "./browser-compute.js";
+
+self.onmessage = async event => {
+  try {
+    const result = await runBrowserEvaluation({
+      ...event.data,
+      onProgress: (done, total) => self.postMessage({type:"progress", done, total})
+    });
+    self.postMessage({type:"complete", result});
+  } catch (error) {
+    self.postMessage({type:"error", message:String(error?.message || error)});
+  }
+};
